3bdcb4095f124381a7c3d481232fff0cd80c15b7
Mritunjay Dubey <127671339+mritunjaydubey00@users.noreply.github.com>
2026-09-20 21:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,12 +1,11 @@
-#include <unordered_map>
 class Solution {
 public:
     int reverseDegree(string s) {
-        unordered_map<char, int> alpha = {{'a', 26},{'b', 25},{'c',24},{'d',23},{'e',22},{'f',21},{'g',20},{'h',19},{'i',18},{'j',17},{'k',16},{'l',15},{'m',14},{'n',13},{'o',12},{'p',11},{'q',10},{'r',9},{'s',8},{'t',7},{'u',6},{'v',5},{'w',4},{'x',3},{'y',2},{'z',1}};
-    int revDeg = 0;
-    for(int i = 0;i<s.length();i++){
-        revDeg = revDeg + ((i+1)*alpha[s[i]]);
+        int n = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            n+=(i+1)*(26-(s[i]-'a'));
+        }
+        return n;
     }
-    return revDeg;
-}
 };
